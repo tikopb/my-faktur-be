8c0a39d4f45af9de6a6fc5e50d5425cfaa391b17
“TIKO <tiko.pb@gmail.com>
2023-08-17 21:05:32 +0700

    init model for partner table

---
+++ internal/repository/partner/partner.go
@@ -0,0 +1,83 @@
+package partner
+
+import (
+	"bemyfaktur/internal/model"
+	"errors"
+
+	"gorm.io/gorm"
+)
+
+type partnerRepo struct {
+	db *gorm.DB
+}
+
+func GetRepository(db *gorm.DB) Repository {
+	return &partnerRepo{
+		db: db,
+	}
+}
+
+// Create implements Repository.
+func (pr *partnerRepo) Create(partner model.Partner) (model.Partner, error) {
+	if err := pr.db.Create(&partner).Error; err != nil {
+		return partner, err
+	}
+	return partner, nil
+}
+
+// Index implements Repository.
+func (pr *partnerRepo) Index() (model.Partner, error) {
+	var data model.Partner
+
+	if err := pr.db.Find(&data).Error; err != nil {
+		return data, err
+	}
+
+	return data, nil
+}
+
+// Show implements Repository.
+func (pr *partnerRepo) Show(id int) (model.Partner, error) {
+	var data model.Partner
+
+	if err := pr.db.Where(model.Partner{ID: id}).Preload("Partner").First(&data).Error; err != nil {
+		return data, errors.New("data not found")
+	}
+	return data, nil
+}
+
+// Update implements Repository.
+func (pr *partnerRepo) Update(id int, updatedPartner model.Partner) (model.Partner, error) {
+	data, err := pr.Show(id)
+
+	if err != nil {
+		return data, err
+	}
+	data.Name = updatedPartner.Name
+	data.CreatedAt = updatedPartner.CreatedAt
+	data.CreatedBy = updatedPartner.CreatedBy
+	data.DNAmount = updatedPartner.DNAmount
+	data.CNAmount = updatedPartner.CNAmount
+	data.Isactive = updatedPartner.Isactive
+
+	//save the data
+	if err := pr.db.Save(&data).Error; err != nil {
+		return data, err
+	}
+
+	return data, nil
+}
+
+// Delete implements Repository.
+func (pr *partnerRepo) Delete(id int) (string, error) {
+	data, err := pr.Show(id)
+	name := data.Name
+
+	if err != nil {
+		return "", err
+	}
+	if err := pr.db.Delete(&data).Error; err != nil {
+		return "", err
+	}
+	return name, nil
+}
---
+++ internal/repository/partner/repository_partner.go
@@ -0,0 +1,27 @@
+package partner
+
+import (
+	"bemyfaktur/internal/model"
+)
+
+type Repository interface {
+	// Index returns a list of Partners.
+	// Call this method to retrieve a list of Partners.
+	Index() (model.Partner, error)
+
+	// Create creates a new Partner.
+	// Call this method to create a new Partner.
+	Create(partner model.Partner) (model.Partner, error)
+
+	// Show retrieves an Partner by its ID.
+	// Call this method to retrieve a specific Partner by ID.
+	Show(id int) (model.Partner, error)
+
+	// Update updates an existing Partner by its ID.
+	// Call this method to update an existing Partner by ID.
+	Update(id int, updatedPartner model.Partner) (model.Partner, error)
+
+	// Delete deletes an Partner by its ID.
+	// Call this method to delete an Partner by ID.
+	Delete(id int) (string, error)
+}
